5e2631e76108b9ac4149ec08072030a9d0b700be
ukhan12 <75581782+ukhan12@users.noreply.github.com>
2021-02-08 00:27:01 -0500

    Add files via upload

---
+++ Topic1.java
@@ -0,0 +1,15 @@
+//Uzma Khan
+//CISC1115 - hw1
+public class Topic1 {
+	public static void main(String[] args) {
+		System.out.println("Uzma Khan");
+		System.out.println();
+		
+		//Use a for loop to print 1-100
+		int num;
+		for(num = 1; num <= 100; num=num+5) {
+		System.out.print(num+"  "+(num+1)+"  "+(num+2)+"  "+(num+3)+"  "+(num+4)+"  ");
+		System.out.println();
+		} 
+	}	
+}
